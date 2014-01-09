456845fd0f6261f9a33ff541192a6d07c142330a
Brian Jurgess <jurgessb@msu.edu>
2014-01-08 21:29:19 -0500

    Adding the print c.recv(1000) line

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
+    print c.recv(1000)
     print 'Got connection from', client_host, client_port
     c.send('HTTP/1.0 200 OK\nDate: ' + time.asctime(time.gmtime()) + '\nContent-Type: text/html\n\n<html>\n<body>\n<h1>Hello, world</h1> this is Brian\'s web server\n</body>\n</html>\n')
     c.close()
