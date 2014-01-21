29052eae20b840569f90aab5202275a2506fa772
Andrew Sundheimer <sundhei4@msu.edu>
2014-01-21 17:47:32 -0500

    hw2 step 1

--- server.py
+++ server.py
@@ -3,24 +3,30 @@ import random
 import socket
 import time
 
-s = socket.socket()
-host = socket.getfqdn()
-port = random.randint(8000, 9999)
-s.bind((host, port))
+def main():
+    clientSocket = socket.socket()
+    host = socket.getfqdn()
+    port = random.randint(8000, 9999)
+    clientSocket.bind((host, port))
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    print 'Starting server on', host, port
 
-s.listen(5)
+    clientSocket.listen(5)
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+    	connection, (client_host, client_port) = clientSocket.accept()
+        print 'Got connection from', client_host, client_port
+        HandleConnection(connection)
 
-    c.send("HTTP/1.0 200 OK\r\n")
-    c.send("Content-type: text/html\r\n")
-    c.send("\r\n")
-    c.send("<h1>Hello, World</h1>")
-    c.send("This is msu-web-dev's Web server")
-    c.close()
+def HandleConnection(connection):
+    connection.send("HTTP/1.0 200 OK\r\n")
+    connection.send("Content-type: text/html\r\n")
+    connection.send("\r\n")
+    connection.send("<h1>Hello, World</h1>")
+    connection.send("This is msu-web-dev's Web server")
+    connection.close()
+
+if __name__ == '__main__':
+    main()
