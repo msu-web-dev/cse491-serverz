8265a887aa1577050dccfae1324aa60a2db13a4f
Andrew Sundheimer <sundhei4@msu.edu>
2014-01-21 17:26:36 -0500

    Fixing hw1 using the solution

--- server.py
+++ server.py
@@ -16,11 +16,12 @@ s.listen(5)
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     c, (client_host, client_port) = s.accept()
+    print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    
-    c.send('HTTP/1.0 200 OK\r\n')
-    c.send('Content-type: text/html\r\n')
-    c.send('\r\n')
-    c.send('<h1>Hello, World.</h1>')
-    c.send('This is msu-web-dev\'s Web server.')
+
+    c.send("HTTP/1.0 200 OK\r\n")
+    c.send("Content-type: text/html\r\n")
+    c.send("\r\n")
+    c.send("<h1>Hello, World</h1>")
+    c.send("This is msu-web-dev's Web server")
     c.close()
