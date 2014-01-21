0cc53f7757e47075baff4388bf20ea40d5ff7f99
Andrew Sundheimer <sundhei4@msu.edu>
2014-01-21 15:14:21 -0500

    Initializing HW2

--- server.py
+++ server.py
@@ -8,16 +8,19 @@ host = socket.getfqdn()
 port = random.randint(8000, 9999)
 s.bind((host, port))
 
-helloWorld = "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n<html><body><h1>Hello World</h1>This is msu-web-dev's Web Server.</body></html>"
-
 print 'Starting server on', host, port
 print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
 s.listen(5)
+
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     c, (client_host, client_port) = s.accept()
-    print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send(helloWorld)
+    
+    c.send('HTTP/1.0 200 OK\r\n')
+    c.send('Content-type: text/html\r\n')
+    c.send('\r\n')
+    c.send('<h1>Hello, World.</h1>')
+    c.send('This is msu-web-dev\'s Web server.')
     c.close()
