c51ef5dcb1c8cd8c4f1127714e73dd9e281553b8
Andrew Sundheimer <sundhei4@msu.edu>
2014-02-20 10:33:26 -0500

    hw6 initial implementation

--- server.py
+++ server.py
@@ -3,59 +3,105 @@ import random
 import socket
 import time
 import urlparse
-import cgi
 import StringIO
-import jinja2
 import sys
 from app import make_app
 import time
+from wsgiref.validate import validator
+
+# import quixote
+# from quixote.demo import create_publisher
+#from quixote.demo.mini_demo import create_publisher
+#from quixote.demo.altdemo import create_publisher
+
+# _the_app = None
+# def make_app():
+#     global _the_app
+#     if _the_app is None:
+#         p = create_publisher()
+#         _the_app = quixote.get_wsgi_app()
+
+#     return _the_app
+
+
+def getRequest(conn):
+    request = ''
+    while True:
+        request_temp = ''
+        try:
+            conn.settimeout(2)
+            request_temp = conn.recv(2048)
+        except:
+            break
+        request += request_temp
+        if len(request_temp) < 2048:
+            break
+    return request
+
 
 def createEnviron(conn):
     environ = {}
+    environ['REQUEST_METHOD'] = ''
+    environ['PATH_INFO'] = ''
+    environ['SERVER_PROTOCOL'] = ''
+    environ['SCRIPT_NAME'] = ''
+    environ['wsgi.input'] = StringIO.StringIO('')
+    environ['QUERY_STRING'] = ''
+    environ['CONTENT_LENGTH'] = '0'
+    environ['CONTENT_TYPE'] = 'text/html'
+    environ['SERVER_NAME'] = ''
+    environ['SERVER_PORT'] = ''
+    environ['wsgi.version'] = ('',)
+    environ['wsgi.errors'] = StringIO.StringIO()
+    environ['wsgi.multithread'] = 0
+    environ['wsgi.multiprocess'] = 0
+    environ['wsgi.run_once'] = 0
+    environ['wsgi.url_scheme'] = 'http'
+    
+    request = getRequest(conn)
+    if request != '':
+        request_headers, request_body = request.split('\r\n\r\n', 1)
 
-    request = ''
-    while True:
-        recv_value = conn.recv(1)
-        request += recv_value
-        if '\r\n\r\n' in request:
-            break
+        headers_string = ''
+        request_line = 0
+        try:
+            request_line, headers_string = request_headers.split('\r\n', 1)
+        except:
+            request_line = request_headers
+            headers_string = ''
 
-    request_headers, request_body = request.split('\r\n\r\n')
+        environ['REQUEST_METHOD'], PATH, \
+        environ['SERVER_PROTOCOL'] = request_line.split(' ')
 
-    try:
-        request_line, headers_string = request_headers.split('\r\n', 1)
-    except:
-        environ['REQUEST_METHOD'], PATH,\
-        environ['SERVER_PROTOCOL'] = request_headers.split(' ')
         PATH = urlparse.urlparse(PATH)
+
         environ['PATH_INFO'] = PATH.path
         environ['QUERY_STRING'] = PATH.query
+        
+        headers = []
+        if headers_string != '':
+            headers = headers_string.split('\r\n')
 
-        return environ
+        headerDict = {}
+        for line in headers:
+            k, v = line.split(': ', 1)
+            headerDict[k.lower()] = v
 
-    environ['REQUEST_METHOD'], PATH, \
-    environ['SERVER_PROTOCOL'] = request_line.split(' ')
+        if 'content-length' in headerDict.keys():
+            environ['CONTENT_LENGTH'] = headerDict['content-length']
+        
+        environ['wsgi.input'] = StringIO.StringIO(request_body)
 
-    PATH = urlparse.urlparse(PATH)
-
-    environ['PATH_INFO'] = PATH.path
-    environ['QUERY_STRING'] = PATH.query
-
-    headers = headers_string.split('\r\n')
-
-    headerDict = {}
-    for line in headers:
-        k, v = line.split(': ', 1)
-        headerDict[k.lower()] = v
-
-    if 'content-length' in headerDict.keys():
-        environ['CONTENT_LENGTH'] = int(headerDict['content-length'])
-        environ['wsgi.input'] = StringIO.StringIO(conn.recv(int(headerDict['content-length'])))
-
-    if 'content-type' in headerDict.keys():
-        environ['CONTENT_TYPE'] = headerDict['content-type']    
-
+        if 'content-type' in headerDict.keys():
+            environ['CONTENT_TYPE'] = headerDict['content-type']
+        
+            
     return environ
+
+
+
+
+
 
 def handle_connection(conn):
     # Get the request and split it to get the
@@ -65,7 +111,6 @@ def handle_connection(conn):
 
     def write(data):
         out = StringIO.StringIO()
-
         if not headers_set:
             raise AssertionError("write() called before start_response()")
         elif not headers_sent:
@@ -82,6 +127,7 @@ def handle_connection(conn):
         if exc_info:
             try:
                 if headers_sent:
+                    print "here"
                     raise exc_info[1].with_traceback(exc_info[2])
             finally:
                 exc_info = None
@@ -90,7 +136,6 @@ def handle_connection(conn):
 
         headers_set[:] = [status, response_headers]
         headers_set[1].append(('Date', time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())))
-        
         return write
 
     wsgi_app = make_app()
@@ -98,16 +143,20 @@ def handle_connection(conn):
     result = wsgi_app(environ, start_response)
 
     try:
-        if result:
-            write(result)
+        for obj in result:
+            if obj:
+                write(obj)
         if not headers_sent:
             write('')
     finally:
         conn.close()
 
-def main():
-    s = socket.socket()         # Create a socket object
-    host = socket.getfqdn() # Get local machine name
+def main(socketmodule = None):
+    if socketmodule is None:
+        socketmodule = socket
+
+    s = socketmodule.socket()         # Create a socket object
+    host = socketmodule.getfqdn() # Get local machine name
     port = random.randint(8000, 9999)
     s.bind((host, port))        # Bind to the port
 
--- test_server.py
+++ test_server.py
@@ -1,6 +1,16 @@
 import sys
 import server
 
+class AcceptCalledMultipleTimes(Exception):
+    pass
+
+class FakeSocketModule(object):
+    def getfqdn(self):
+        return "fakehost"
+
+    def socket(self):
+        return FakeConnection("")
+
 class FakeConnection(object):
     """
     A fake connection class that mimics a real TCP socket for the purpose
@@ -10,6 +20,24 @@ class FakeConnection(object):
         self.to_recv = to_recv
         self.sent = ""
         self.is_closed = False
+        self.timeout = 0
+        self.n_times_accept_called = 0
+
+    def bind(self, param):
+        (host, port) = param
+
+    def listen(self, n):
+        assert n == 5
+        if n != 5:
+            raise Exception("n should be 5 you dummy")
+
+    def accept(self):
+        if self.n_times_accept_called >= 1:
+            raise AcceptCalledMultipleTimes("stop calling accept, please")
+        self.n_times_accept_called += 1
+
+        c = FakeConnection("")
+        return c, ("noclient", 32351)
 
     def recv(self, n):
         if n > len(self.to_recv):
@@ -25,6 +53,22 @@ class FakeConnection(object):
 
     def close(self):
         self.is_closed = True
+
+    def settimeout(self, d):
+        self.timeout = d
+
+
+def test_main():
+    fakemodule = FakeSocketModule()
+
+    success = False
+    try:
+        server.main(fakemodule)
+    except AcceptCalledMultipleTimes:
+        success = True
+        pass
+
+    assert success, "something went wrong"
 
 # Test a basic GET call.
 
@@ -52,7 +96,7 @@ def test_file():
     server.handle_connection(conn)
 
     assert 'HTTP/1.0 200 OK\r\n' in conn.sent, 'Got: %s' % (repr(conn.sent),)
-    assert '<h1>File page!</h1>' in conn.sent, 'Got: %s' % (repr(conn.sent),)
+    assert '<iframe src="/file.txt" type="text/plain"></iframe>' in conn.sent, 'Got: %s' % (repr(conn.sent),)
 
 def test_image():
     conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
@@ -60,7 +104,7 @@ def test_image():
     server.handle_connection(conn)
 
     assert 'HTTP/1.0 200 OK\r\n' in conn.sent, 'Got: %s' % (repr(conn.sent),)
-    assert '<h1>Image page!</h1>' in conn.sent, 'Got: %s' % (repr(conn.sent),)
+    assert '<img src="/img.jpg" />' in conn.sent, 'Got: %s' % (repr(conn.sent),)
 
 def test_post():
     conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
@@ -129,3 +173,33 @@ def test_multipart_form():
 
     assert 'HTTP/1.0 404 NOT FOUND' in conn.sent, 'Got: %s' % (repr(conn.sent),)
     assert '<h1>404 NOT FOUND</h1>' in conn.sent, 'Got: %s' % (repr(conn.send),)
+
+def test_jpg_serve():
+    conn = FakeConnection("GET /img.jpg HTTP/1.0\r\n\r\n")
+
+    server.handle_connection(conn)
+
+    fp = open('./img.jpg', 'rb')
+    d = fp.read()
+    fp.close()
+
+    assert d in conn.sent
+
+def test_file_serve():
+    conn = FakeConnection("GET /file.txt HTTP/1.0\r\n\r\n")
+
+    server.handle_connection(conn)
+
+    fp = open('./file.txt', 'rb')
+    d = fp.read()
+    fp.close()
+
+    assert d in conn.sent
+
+def test_non_existant_file():
+    conn = FakeConnection("GET /ddd.txt HTTP/1.0\r\n\r\n")
+
+    server.handle_connection(conn)
+
+    assert 'HTTP/1.0 404 NOT FOUND' in conn.sent, 'Got: %s' % (repr(conn.sent),)
+    assert '<h1>404 NOT FOUND</h1>' in conn.sent, 'Got: %s' % (repr(conn.sent),)
